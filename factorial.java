01403e7bff6905b9f1569189f8974c905c8b5a1c
kaaviyas1906 <83899413+kaaviyas1906@users.noreply.github.com>
2021-08-28 13:00:08 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
-import jdk.internal.org.jline.terminal.Terminal.MouseTracking;
 
-public class factorial {
+class fact {
   int tofindfac(int n){
     if ((n == 0) || (n == 1)) {
       return 1;
     } else {
       return (n * tofindfac(n - 1));
     }
-  }MouseTracking
- 
   }
+ 
+}
 
-
-  public static void main(String[] args) {
+ public class factorial{
+ public static void main(String[] args) {
     System.out.println("Enter a number:");
     //int fact = 1;
     Scanner tofac = new Scanner(System.in);
@@ -25,4 +24,5 @@ public class factorial {
     System.out.println(result);
     tofac.close();
   }
-}
+ }
+  
